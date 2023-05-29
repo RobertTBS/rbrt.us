ff94eef764550996cfdb09a7baf94ecc62df72b4
Glitch (rbrt-web) <none>
2023-05-29 22:07:58 +0000

    Time until end

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-if (window.location.toString().substring(0,14) == "https://rbrt-web.g"){alert("You are using the test site!")}
+if (window.location.toString().substring(0,18) == "https://rbrt-web.g"){alert("You are using the test site!")}
 function timeto(){
 let time = 0;
 let now = new Date()
@@ -8,3 +8,17 @@ let days = time_difference / (1000 * 60 * 60 * 24);
 document.getElementById("time").innerHTML = Math.floor(days)+2+" days"
 }
 timeto()
+
+function schoolend(){
+const currentDate = new Date();
+const targetDate = new Date("May 31, 2023 13:15:00");
+const timeDiff = targetDate.getTime() - currentDate.getTime();
+
+const Day = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+const Hour = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+const Min = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+const Sec = Math.floor((timeDiff % (1000 * 60)) / 1000);
+document.getElementById("schoolend").innerHTML = Day+" days, "+Hour+" hours, "+Min+" minutes, and "+Sec+" seconds"
+}
+schoolend()
+setInterval(schoolend,1000)
