22a99dbe5b043e697550e8e648c2e2d1a479be02
Glitch (rbrt-web) <none>
2023-05-30 18:34:17 +0000

    School start countdown

--- main.js
+++ main.js
@@ -18,7 +18,20 @@ const Day = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
 const Hour = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 const Min = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
 const Sec = Math.floor((timeDiff % (1000 * 60)) / 1000);
-document.getElementById("schoolend").innerHTML = Day+" days, "+Hour+" hours, "+Min+" minutes, and "+Sec+" seconds"
+document.getElementById("schoolend").innerHTML = Hour+" hours, "+Min+" minutes, and "+Sec+" seconds"
+}
+function schoolstart(){
+const currentDate = new Date();
+const targetDate = new Date("August 16, 2023");
+const timeDiff = targetDate.getTime() - currentDate.getTime();
+
+const Day = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+const Hour = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+const Min = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+const Sec = Math.floor((timeDiff % (1000 * 60)) / 1000);
+document.getElementById("schoolstart").innerHTML = Day+" days, "+Hour+" hours, "+Min+" minutes, and "+Sec+" seconds"
 }
 schoolend()
+schoolstart()
 setInterval(schoolend,1000)
+setInterval(schoolstart,1000)
