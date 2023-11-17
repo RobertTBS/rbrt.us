b840d48ce61a2249f8e38a95123e17eedb03aba1
Glitch (robertweb-test) <none>
2023-11-17 05:37:01 +0000

    Class Ending Notifier 3

---
+++ classends3/index.js
@@ -0,0 +1,159 @@
+Notification.requestPermission(); //TODO TOMORROW
+const Table = document.getElementById("output");
+let MultiArray = JSON.parse(document.getElementById('ArrTransfer').innerHTML); //Make a new array we can edit
+
+let startP = 10;
+document.getElementById("startP").value=10;
+let showLunches = document.getElementById("LunchToggle").checked ||true;
+let simple = document.getElementById("AllPeriodsToggle").checked||false;
+let dateType = "NORM";
+let notify = document.getElementById("Notify").checked || false;
+const select = document.getElementById("startP");
+
+async function wakelock(){     //Chrome wake lock
+  let wakeLock = null;
+  try {
+    wakeLock = await navigator.wakeLock.request('screen');
+    //alert('Wake Lock is active!');
+  } catch (err) {
+    // The Wake Lock request has failed - usually system related, such as battery.
+    console.log(`${err.name}, ${err.message}`);
+  }
+}
+wakelock()
+
+
+function makeDate(givenDate){
+  const Hr = givenDate.toString().split(",")[3] | 0;
+  const Min = givenDate.toString().split(",")[4] | 0;
+  const now = new Date();
+  return new Date(now.getFullYear().toString()+"/"+(now.getMonth()+1).toString()+"/"+now.getDate().toString()+", "+Hr.toString()+":"+Min.toString())
+}
+if (new Date().getDay() == 1){
+  dateType = "LS";
+  document.getElementById("TypeSelect").value = "LS";
+}
+
+// Updte the data when it is changed
+const elementToObserve = document.getElementById('ArrTransfer');
+const observer = new MutationObserver(() => {
+  MultiArray = JSON.parse(document.getElementById('ArrTransfer').innerHTML);
+  calculate();
+});
+observer.observe(elementToObserve, { subtree: true, childList: true });
+
+
+function calculate(){ //Runs every time the data element is changed, about once per second.
+  document.getElementById("REMOVABLE").remove()
+  MultiArray.forEach((innerArr, indexX) => {
+    //console.log(("TYPE-"+innerArr[0]+"|PD-"+innerArr[1]))
+    document.getElementById("TYPE-"+innerArr[0]+"|PD-"+innerArr[1]).parentElement.style.display='none'
+  });
+  
+  MultiArray.forEach((innerArr, indexX) => {
+    innerArr.forEach((val,indexY) => {
+      if (innerArr[0] == dateType && innerArr[indexY] != dateType){
+        //console.log(innerArr[indexY])
+        if (showLunches == true){ //Show if lunches available
+          document.getElementById("TYPE-"+dateType+"|PD-"+innerArr[1]).parentElement.style.display='block';
+        }else if (innerArr[1].charAt(0) != 'L' ){
+          document.getElementById("TYPE-"+dateType+"|PD-"+innerArr[1]).parentElement.style.display='block';
+        }
+      }
+    })
+  });
+
+  let least = [0,999999999] //PD, MS
+  MultiArray.forEach((innerArr, indexX) => {
+    innerArr.forEach((text,indexY) => {
+      if ((innerArr[startP])<least[1] && least[1] > 0 && innerArr[startP] < 0){
+        least[0] = innerArr[1];
+        least[1] = innerArr[startP];
+      }
+    })
+    
+    if (innerArr[1] == least[0]){
+        
+      //document.title = (innerArr[1]+" "+innerArr[10])
+        
+       //alert(least[0])
+      if (least[0] == 0){
+        least[0] = innerArr.length-4
+
+      }else{
+        least[0] = innerArr.length-4
+      }
+      
+      //console.log(dateType+" "+least[0])
+      document.getElementById("TYPE-"+dateType+"|PD-"+least[0]).parentElement.style.backgroundColor='green';
+      if (simple){
+        let children = document.getElementById("TYPE-"+dateType+"|PD-"+least[0]).parentElement.parentElement.getElementsByTagName("tr")
+        for(var i=0; i<children.length; i++) {
+          children[i].style.display = 'none';
+        }
+        document.getElementById("TYPE-"+dateType+"|PD-"+least[0]).parentElement.style.fontSize = '9.5vw';
+        document.getElementById("TYPE-"+dateType+"|PD-"+least[0]).parentElement.style.fontWeight = '900';
+        document.getElementById("TYPE-"+dateType+"|PD-"+least[0]).parentElement.style.display = 'block';
+        document.getElementById("TYPE-"+dateType+"|PD-"+least[0]).parentElement.children[0].innerHTML="";
+        document.getElementById("TYPE-"+dateType+"|PD-"+least[0]).parentElement.children[1].innerHTML="";
+        if (startP == 10){
+          document.getElementById("TYPE-"+dateType+"|PD-"+least[0]).parentElement.children[2].innerHTML="";
+          document.getElementById("TYPE-"+dateType+"|PD-"+least[0]).parentElement.children[3].innerHTML = document.getElementById("TYPE-"+dateType+"|PD-"+least[0]).parentElement.children[3].innerHTML.toString().split("S")[0]+"S"
+        };
+        //console.log(document.getElementById("TYPE-"+dateType+"|PD-"+least[0]).parentElement.children[2].innerHTML.toString())
+        if (!startP == 9){
+          document.getElementById("TYPE-"+dateType+"|PD-"+least[0]).parentElement.children[3].innerHTML="";
+          document.getElementById("TYPE-"+dateType+"|PD-"+least[0]).parentElement.children[2].innerHTML = document.getElementById("TYPE-"+dateType+"|PD-"+least[0]).parentElement.children[2].innerHTML.toString().split("S")[0]+"S"
+        };
+        document.getElementById("TYPE-"+dateType+"|PD-"+least[0]).parentElement.style.textAlign = "center";
+        document.getElementById("TYPE-"+dateType+"|PD-"+least[0]).parentElement.parentElement.style.width = '100%';
+        document.getElementById("TYPE-"+dateType+"|PD-"+least[0]).parentElement.style.backgroundColor='rgba(0,0,0,0)';
+      }
+    }
+  });
+  let children = document.getElementById("TYPE-"+dateType+"|PD-"+least[0]).parentElement.children;
+  let title = children[3].innerHTML.split("S")[0]+"S";
+  let pd = children[1].innerHTML.split("&nbsp;")[0];
+  document.title = title;
+  
+  function notif(){
+    const notification = new Notification(title,{tag:'notif',body:"Period "+pd+" ends in "+title, silent:true, renotify: true, icon:'https://cdn.glitch.com/782eb81c-c672-44cf-b500-e4669b3dc4f1/NORTHPOLK.svg',badge:'https://cdn.glitch.com/782eb81c-c672-44cf-b500-e4669b3dc4f1/NORTHPOLK.svg'});  
+  }
+         
+  if (notify==true){
+  if (!("Notification" in window)) {alert("This browser does not support desktop notification")} //Check for support
+  else if (Notification.permission === "granted") { //Build notification
+    notif()
+  }else if (Notification.permission !== "denied") { //Request permission again
+    Notification.requestPermission().then((permission) => {
+      if (permission === "granted") {
+        notif()
+      }
+    });
+  }
+}
+  let str = ('td'); //TODO
+  let allTds = document.querySelector(".output").querySelectorAll(str)
+  allTds.forEach((text, index) => {
+    if (text.innerHTML.includes("S ")){
+      text.innerHTML=text.innerHTML.split("S ")[0]+"S"
+    }
+
+    if (startP == 10){
+      for (let i = 2; i < allTds.length ;i=i+2){
+        allTds[i].innerHTML = "";
+      }
+    }else if (startP == 9){
+      for (let i = 3; i < allTds.length ;i=i+4){
+        allTds[i].innerHTML = "";
+      }
+    }
+      
+//    }
+      //console.log(allTds[9].innerHTML)
+    if (text.innerHTML.toString().includes(dateType) || (startP == 9 && text.toString().includes(startP))){
+      text.innerHTML = "";
+    }
+  });
+  
+}
---
+++ classends3/main.js
@@ -0,0 +1,169 @@
+//This is an untheftable program. It is so stupid and complicated that even I don't fully know whats going on.
+
+//GOOGLE SHEETS FETCH
+const sheetId = '14Q-XX0Vf2wf1Da-Gdc_d5F3tHVJTAg5V-vH2RWS9f0A';
+const base = `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?`;
+const sheetName = 'CEN 3 TIME SHEET';
+const query = encodeURIComponent('Select *')
+const url = `${base}&sheet=${sheetName}&tq=${query}`
+const data = []
+document.addEventListener('DOMContentLoaded', init)
+const output = document.querySelector('.output')
+const dataTransfer = document.getElementById('ArrTransfer')
+
+
+let Times = [[0]];
+let validDates = [];
+
+function init() {
+    fetch(url)
+        .then(res => res.text())
+        .then(rep => {
+            //Remove additional text and extract only JSON:
+            const jsonData = JSON.parse(rep.substring(47).slice(0, -2));
+            //console.log(rep)
+            const colz = [];
+            //const tr = document.createElement('tr');
+            //Extract column labels
+            jsonData.table.cols.forEach((heading) => {
+                if (heading.label) {
+                    let column = heading.label;
+                    colz.push(column);
+                    //const th = document.createElement('th');
+                    //th.innerText = column;
+                    //tr.appendChild(th);
+                }
+            })
+            //output.appendChild(tr);
+            //extract row data:
+            jsonData.table.rows.forEach((rowData) => {
+                const row = {};
+                colz.forEach((ele, ind) => {
+                    row[ele] = (rowData.c[ind] != null) ? rowData.c[ind].v : '';
+                })
+                data.push(row);
+            })
+            processRows(data);
+        })
+}
+function processRows(json) {
+  json.forEach((row,indexX) => {
+//  const tr = document.createElement('tr');
+    const keys = Object.keys(row);
+    let tempArr = [];
+    keys.forEach((key,indexY) => {
+//    const td = document.createElement('td');
+      tempArr[tempArr.length] = 0;
+      tempArr[tempArr.length+1] = 0; 
+      tempArr[indexY] = row[key];
+//    td.textContent = row[key];
+//    tr.appendChild(td);
+    })
+    Times[indexX]=tempArr;
+//  output.appendChild(tr);        
+  })
+  setDate()
+}
+
+//DATE CALCULATIONS
+function makeDate(givenDate){
+  const Hr = givenDate.toString().split(",")[3] | 0;
+  const Min = givenDate.toString().split(",")[4] | 0;
+  const now = new Date();
+  return new Date(now.getFullYear().toString()+"/"+(now.getMonth()+1).toString()+"/"+now.getDate().toString()+", "+Hr.toString()+":"+Min.toString())
+}
+function timeBetween(end,start){
+  const StMs = start.getTime();
+  const EdMs = end.getTime();
+  const MsTo = EdMs-StMs;
+    var hours = MsTo/3.6e6 | 0;
+    var minutes  = MsTo%3.6e6 / 6e4 | 0;
+    var seconds  = Math.round(MsTo%6e4 / 1e3);
+  return [0,hours,minutes,seconds,MsTo];
+}
+function CALCDATE(startInitial,endInitial){
+  const TimeArr = timeBetween(makeDate(startInitial),makeDate(endInitial));
+  return /*TimeArr[0]+"D "+*/TimeArr[1]+"H "+TimeArr[2]+"M "+TimeArr[3]+"S ";
+}
+function START(st,end){
+  const TimeArr = timeBetween(makeDate(st),new Date());
+  return /*TimeArr[0]+"D "+*/TimeArr[1]+"H "+TimeArr[2]+"M "+TimeArr[3]+"S ";
+}
+function END(st,end){
+  const TimeArr = timeBetween(makeDate(end), new Date());
+  return /*TimeArr[0]+"D "+*/TimeArr[1]+"H "+TimeArr[2]+"M "+TimeArr[3]+"S ";
+}
+//CALCULATIONS AND ADD TO SCREEN
+function isDateValid(dateStr) {
+  return !isNaN(new Date(dateStr));
+}
+function setDate(){
+  Times.forEach( (innerArr,indexX) => {
+    innerArr.forEach((text,indexY) => {
+      if (text.toString().includes("Date")){
+        Times[indexX][indexY+3] = makeDate(text);
+      }
+    })
+  })
+  calc()
+}
+function calc(){
+  output.innerHTML = '<tr id="REMOVABLE" style="background-color:#e74c3c;color:black;"><td>NORMAL DAY - 8:15 - 3:15</td><td>Period name</td><td>Time until start</td><td>Time until end</td></tr>';
+  Times.forEach( (val,indexX) => {
+    if (isDateValid(val[5])){
+      //val[2]let to2 = CALCDATE(val[3],val[2]);
+      //console.log(val[2])
+      val[7] = val[2];
+      val[8] = val[3];
+      val[2] = START(val[2],val[3]);
+      val[3] = END(val[2],val[3]);
+      val[9] = new Date().getTime() - new Date(val[5]).getTime();
+      val[10] = new Date().getTime() - new Date(val[6]).getTime();
+    }
+  })  
+  
+  addToScreen()
+}
+
+function addToScreen(){
+  Times.forEach( (innerArr,index) => {
+    let ZeroVal;
+    const tr = document.createElement('tr');
+    innerArr.forEach((text,index) => {
+      if (index == 0){
+        ZeroVal = text;
+        if (text.charAt(0) == "."){
+          text = text.substring(1);
+          innerArr[2]='Period name';
+          innerArr[3]='Time until start';
+          innerArr[4]='Time until end';
+          innerArr[5]="ISLABEL"
+        }
+      }
+      if (text != 0 && index < 5){
+        const td = document.createElement('td');
+          td.textContent = text+"\xa0\xa0\xa0\xa0\xa0\xa0";
+        if (innerArr[5]=="ISLABEL"){
+          td.style.backgroundColor='#e74c3c';
+          td.style.color='black';
+        }
+        tr.appendChild(td);
+        td.setAttribute("id", "TYPE-"+innerArr[0]+"|PD-"+innerArr[1])
+      }
+    })
+    /*if (ZeroVal.charAt(0) == "."){              //Uncomment if you dont add the lable to each day, currently on line 127 / 12 before this one
+      const td = document.createElement('td');
+      td.textContent = '';
+      tr.appendChild(td);
+    }*/
+    output.appendChild(tr);
+    
+    
+    innerArr[2]=innerArr[7];
+    innerArr[3]=innerArr[8]
+  })
+  
+  dataTransfer.innerHTML = JSON.stringify(Times);
+}
+
+setInterval(calc,1000) 
